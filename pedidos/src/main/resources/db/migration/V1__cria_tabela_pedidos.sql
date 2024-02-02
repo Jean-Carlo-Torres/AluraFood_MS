5f4de1c16e9142acd3d941facc1cbdc2246a7cf2
Jean Carlo <jeansanto2002@gmail.com>
2024-02-02 16:12:07 -0300

    Incluindo o microsserviço de pedidos

---
+++ pedidos/src/main/resources/db/migration/V1__cria_tabela_pedidos.sql
@@ -0,0 +1,5 @@
+CREATE TABLE pedidos (
+    id bigserial PRIMARY KEY,
+    data_hora timestamp NOT NULL,
+    status varchar(255) NOT NULL
+);
---
+++ pedidos/src/main/resources/db/migration/V2__cria_tabela_item_pedido.sql
@@ -0,0 +1,7 @@
+CREATE TABLE item_do_pedido (
+    id bigserial PRIMARY KEY,
+    descricao varchar(255),
+    quantidade int NOT NULL,
+    pedido_id bigint NOT NULL,
+    FOREIGN KEY (pedido_id) REFERENCES pedidos(id)
+);
