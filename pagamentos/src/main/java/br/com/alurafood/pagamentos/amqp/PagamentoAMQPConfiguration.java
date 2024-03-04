eefe1f808d5e84633ffefa7d9b0aaf4e7c846f33
Jean Carlo <jeansanto2002@gmail.com>
2024-03-04 14:51:48 -0300

    Criando a conexão com RabbitMQ

---
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/amqp/PagamentoAMQPConfiguration.java
@@ -0,0 +1,29 @@
+package br.com.alurafood.pagamentos.amqp;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PagamentoAMQPConfiguration {
+
+    @Bean
+    public Queue criaFila() {
+        return QueueBuilder.nonDurable("Pagamento concluído").build();
+    }
+
+    @Bean
+    public RabbitAdmin criaAdmin(ConnectionFactory conn){
+        return new RabbitAdmin(conn);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent>  inicializacao(RabbitAdmin admin){
+        return event -> admin.initialize();
+    }
+}
