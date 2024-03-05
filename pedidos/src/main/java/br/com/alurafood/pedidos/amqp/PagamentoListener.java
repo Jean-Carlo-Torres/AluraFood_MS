52eda0ad88e6e7ca63ece8adc26636d466bd0f28
Jean Carlo <jeansanto2002@gmail.com>
2024-03-05 16:48:36 -0300

    Contornando erros com DLQs

--- pedidos/src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
+++ pedidos/src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
@@ -9,6 +9,13 @@ public class PagamentoListener {
 
     @RabbitListener(queues = "pagamentos.detalhes-pedido")
     public  void recebendoMensagem(PagamentoDto pagamento){
+        System.out.println(pagamento.getId());
+        System.out.println(pagamento.getNumero());
+
+        if (pagamento.getNumero().equals("0000")) {
+            throw new RuntimeException("não consegui processar");
+        }
+
         String mensagem = """
                 Dados do pagamento: %s
                 Número do Pedido: %s
